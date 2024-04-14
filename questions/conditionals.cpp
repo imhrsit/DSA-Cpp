f0bdbdb2740f4c2105f4a3067b2c48b740f8564b
imhrsit <tiwariharsh1408@gmail.com>
2024-04-15 00:36:14 +0530

    dsa yaaarrrr

---
+++ questions/conditionals.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter a value"<<endl;
+    cin >> ch;
+    int ascii = ch;
+
+    if(ascii>=65 && ascii<=90){
+        cout << "Upper Case" << endl;
+    }
+    else if(ascii>=97 && ascii<=122){
+        cout << "Lower Case" << endl;
+    }
+    else if(ascii>=48 && ascii <=57){
+        cout << "Numerical" << endl;
+    }
+    else{
+        cout << "something else" << endl;
+    }
+}
