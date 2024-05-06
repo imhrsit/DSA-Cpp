8f8e454ab57471a64b8702049988eedd548e5bc1
imhrsit <tiwariharsh1408@gmail.com>
2024-05-06 22:39:27 +0530

    sherr

---
+++ arrays/swapAlternate.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void swapAlternate(int arr[], int n){
+    for(int i=0; i<n; i+=2){
+        if(i+1 < n){
+            swap(arr[i], arr[i+1]);
+        }
+    }
+}
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[5] = {1,2,7,8,5};
+    int brr[6] = {1,2,3,4,5,6};
+
+    swapAlternate(arr, 5);
+    swapAlternate(brr, 6);
+
+    printArray(arr, 5);
+    printArray(brr, 6);
+}
