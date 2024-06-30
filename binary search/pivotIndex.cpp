866c38d61993ba83ea26e8487992d0d444e13b6a
imhrsit <tiwariharsh1408@gmail.com>
2024-07-01 00:28:32 +0530

    BS

--- binary search/pivotIndex.cpp
+++ binary search/pivotIndex.cpp
@@ -1,49 +1,23 @@
 #include<iostream>
 using namespace std;
 
-//time complexity crying in corner...
-
-int leftSum(int arr[], int size){
+int getPivot(int arr[], int n){
     int s=0;
-    int e=size-1;
-    int mid=s+(e-s)/2;
-    int sum=0;
+    int e=n-1;
+    int mid= s=(e-s)/2;
 
-    for(int i=0; i<mid; i++){
-        sum=i+(i-1);
+    while(s<e){
+        if(arr[mid] >= arr[0]){
+            s=mid+1;
+    } else{
+        e=mid;
     }
-    return sum;
-}
-
-int rightSum(int arr[], int size){
-    int s=0;
-    int e=size-1;
-    int mid=s+(e-s)/2;
-    int sum=0;
-
-    for(int i=mid+1; i<=size; i++){
-        sum=i+(i-1);
+        mid= s=(e-s)/2;
     }
-    return sum;
-}
-
-int pivotIndex(int arr[], int size){
-    int s=0;
-    int e=size-1;
-    int mid=s+(e-s)/2;
-
-    while(s<=e){
-        if(leftSum==rightSum){
-            return mid;
-        }
-        else{
-            e=mid-1;
-        }
-    }
-    return -1;
+    return s;
 }
 
 int main(){
-    int arr[6] = {1,7,3,6,5,6};
-    cout<<"Pivot Index of the array is "<<pivotIndex<<endl;
+    int brr[5] = {7,9,1,2,3};
+    cout<<"pivot index is "<<getPivot(brr, 5)<<endl;
 }
