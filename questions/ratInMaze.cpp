c55198c65fd4a58102f5a87b04cdc42690c6fd86
imhrsit <tiwariharsh1408@gmail.com>
2024-07-29 17:23:58 +0530

    rat in a maze

---
+++ questions/ratInMaze.cpp
@@ -0,0 +1,116 @@
+// //{ Driver Code Starts
+// // Initial template for C++
+
+// #include <iostream>
+// using namespace std;
+
+
+// // } Driver Code Ends
+// // User function template for C++
+
+// class Solution {
+// private:
+
+//     bool isSafe(int x, int y, vector<vector<int>> &mat, vector<vector<int>> visited){
+//         if((x>=0 && x<n) && (y>=0 && y<n) && visited[x][y]==0 && mat[x][y]==1){
+//             return true;
+//         }else {
+//         return false;   
+//         }
+//     }
+
+//     void solve(vector<vector<int>> &mat, vector<string>& ans, int x, int y, vector<vector<int>> visited, string path){
+//         //base case
+//         if(x==n-1 && y==n-1){
+//             ans.push_back(path);
+//             return;
+//         }
+//         visited[x][y] = 1;
+        
+//         //4 choises - D,L,U,R
+//         //down
+//         int newx = x+1;
+//         int newy = y;
+//         if(isSafe(newx, newy, mat, visited)){
+//             path.push_back("D");
+//             solve(mat, ans, newx, newy, visited, path);
+//             path.pop_back();
+//         }
+//         //left
+//         int newx = x;
+//         int newy = y-1;
+//         if(isSafe(newx, newy, mat, visited)){
+//             path.push_back("L");
+//             solve(mat, ans, newx, newy, visited, path);
+//             path.pop_back();
+//         }
+//         //right
+//         int newx = x;
+//         int newy = y+1;
+//         if(isSafe(newx, newy, mat, visited)){
+//             path.push_back("R");
+//             solve(mat, ans, newx, newy, visited, path);
+//             path.pop_back();
+//         }
+//         //up
+//         int newx = x-1;
+//         int newy = y;
+//         if(isSafe(newx, newy, mat, visited)){
+//             path.push_back("U");
+//             solve(mat, ans, newx, newy, visited, path);
+//             path.pop_back();
+//         }
+        
+//         visited[x][y] = 0;
+        
+//     }
+//   public:
+//     vector<string> findPath(vector<vector<int>> &mat) {
+//         vector<string> ans;
+//         if(mat[0][0] == 0){
+//             return ans;
+//         }
+        
+//         int srcx = 0;
+//         int srcy = 0;
+//         //visited array initialize with 0
+//         vector<vector<int>> visited = m;
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
+//                 visited[i][j] = 0;
+//             }
+//         }
+//         string path = "";
+//         solve(mat, ans, srcx, srcy, visited, path);
+//         sort(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
+
+
+// //{ Driver Code Starts.
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int n;
+//         cin >> n;
+//         vector<vector<int>> m(n, vector<int>(n, 0));
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 cin >> m[i][j];
+//             }
+//         }
+//         Solution obj;
+//         vector<string> result = obj.findPath(m);
+//         sort(result.begin(), result.end());
+//         if (result.size() == 0)
+//             cout << -1;
+//         else
+//             for (int i = 0; i < result.size(); i++)
+//                 cout << result[i] << " ";
+//         cout << endl;
+//     }
+//     return 0;
+// }
