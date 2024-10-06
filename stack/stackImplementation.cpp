5b569d84f63eccd8e1690413fd89f9a77dfbdeaa
imhrsit <tiwariharsh1408@gmail.com>
2024-10-06 14:54:18 +0530

    stack added

---
+++ stack/stackImplementation.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+class Stack{
+    //properties
+    public:
+    int *arr;
+    int top;
+    int size;
+
+    //behaviour
+    Stack(int size){
+        this -> size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element){
+        if(size-top > 1){
+            top++;
+            arr[top] = element;
+        }else{
+            cout<<"Stack Overflow"<<endl; 
+        }
+    }
+    void pop(){
+        if(top >= 0){
+            top--;
+        }else{
+            cout<<"Stack Underflow"<<endl; 
+        }
+    }
+    int peek(){
+        if(top>=0)
+            return arr[top];
+        else{
+            cout<<"stack is empty"<<endl;
+            return -1; 
+        }
+    }
+    bool isEmpty(){
+        if(top == -1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
+
+int main(){
+    Stack st(5);
+
+    st.push(22);
+    st.push(43);
+    st.push(44);
+    st.push(22);
+    st.push(43);
+
+    cout<< st.peek() <<endl;
+    st.pop();
+    cout<< st.peek() <<endl;
+    st.pop();
+    cout<< st.peek() <<endl;
+    st.pop();
+    cout<< st.peek() <<endl;
+
+    if(st.isEmpty()){
+        cout<<"stack kahli hai"<<endl;
+    }else{
+        cout<<"stack kahli nahi hai"<<endl;
+    }
+}
