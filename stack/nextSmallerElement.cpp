77e3ab88423604d536b078a21cd6bfc65fb5ad21
imhrsit <tiwariharsh1408@gmail.com>
2024-12-06 01:41:32 +0530

    next smaller element

---
+++ stack/nextSmallerElement.cpp
@@ -0,0 +1,19 @@
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+// int main(){
+    // stack<int> st;
+    // st.push(-1);
+    // vector<int> ans(n); 
+
+    // for(int i=n-1; i>=0; i--){
+    //     int curr = arr[i];
+    //     while(st.top() >= curr){
+    //         st.pop();
+    //     }
+    //     //ans is stack ka top
+    //     ans[i] = st.top();
+    //     st.push(curr);
+    // }
+    // return ans;
+// }
